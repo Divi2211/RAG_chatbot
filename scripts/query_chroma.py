5ee761fe0ebd4bda2ede6cd7cd4facdefc041200
Divija <160344076+Divi2211@users.noreply.github.com>
2025-06-19 12:24:29 +0530

    Update query_chroma.py

--- scripts/query_chroma.py
+++ scripts/query_chroma.py
@@ -1,17 +1,19 @@
-from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
 import os
 
 def search_chroma(query: str, top_k=3):
-    hf_token = os.getenv("HF_API_TOKEN")
-    embeddings = HuggingFaceInferenceAPIEmbeddings(
-        api_key=hf_token,
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-ada-002",
+        openai_api_key=os.getenv("OPENROUTER_API_KEY"),
+        openai_api_base="https://openrouter.ai/api/v1"
     )
+
     db = Chroma(
         persist_directory="./chroma_store",
         embedding_function=embeddings
     )
+
     results = db.similarity_search_with_score(query, k=top_k)
     response = []
     for doc, score in results:
